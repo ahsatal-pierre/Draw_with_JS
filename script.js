5220f02a7d54c0c1efbd6160a8f8f65c3da95f6e
PIERRE AHSATAL <asathal.pierre@gmail.com>
2023-05-14 17:45:23 +0200

    start to draw after a click

--- script.js
+++ script.js
@@ -1,56 +1,67 @@
-let color = '#3A8BBA'; 
-let size = 16;
+let color = '#3A8BBA';
+let size = 64;
+let isGridClickable = false;
 
 function createGrid(size) {
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for (let i = 0; i < size*size; i++) {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('box');
-        container.appendChild(newDiv);
-        newDiv.addEventListener('mouseover', hoverChangeColor);
-    }
+  for (let i = 0; i < size * size; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('box');
+    container.appendChild(newDiv);
+  }
+
+  container.addEventListener('click', startDrawing);
 }
+
+function startDrawing() {
+  if (!isGridClickable) {
+    container.querySelectorAll('.box').forEach((box) => {
+      box.addEventListener('mouseover', hoverChangeColor);
+    });
+    isGridClickable = true;
+  }
+}
+
 
 function hoverChangeColor(event) {
-     event.target.style.backgroundColor = color;
+  if (isGridClickable) {
+    event.target.style.backgroundColor = color;
+  }
 }
 
-// rainbow color 
+// rainbow color
 const buttonColor = document.getElementById('rainbowColor');
 buttonColor.addEventListener('click', rainbowColor);
 
-
 function rainbowColor() {
-    const colorR = Math.floor(Math.random() * 256);
-    const colorG = Math.floor(Math.random() * 256);
-    const colorB = Math.floor(Math.random() * 256);
-    newColor = `rgb(${colorR}, ${colorG}, ${colorB})`
-    setColor();
+  const colorR = Math.floor(Math.random() * 256);
+  const colorG = Math.floor(Math.random() * 256);
+  const colorB = Math.floor(Math.random() * 256);
+  newColor = `rgb(${colorR}, ${colorG}, ${colorB})`;
+  setColor();
 }
 
 function setColor() {
-    color = newColor;
+  color = newColor;
 }
 
 // new size
-
-function setSize() {
-    size = newSize;
-    createGrid(size);
+function setSize(newSize) {
+  size = newSize;
+  container.innerHTML = '';
+  createGrid(size);
 }
 
 const resetSize = document.getElementById('resetSize');
 resetSize.addEventListener('click', newGrid);
 
 function newGrid() {
-    newSize = prompt('how large?');
-    container.innerHTML = '';
-    setSize();
+  const newSize = prompt('Enter the new grid size:');
+  if (newSize !== null) {
+    setSize(parseInt(newSize));
+  }
 }
-
-
-
 
 createGrid(size);
